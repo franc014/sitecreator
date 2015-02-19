f91afd215cc239cbb24c4808a75f36639a1d02d5
JUan Andrade <jfandradea@gmail.com>
2015-02-19 12:21:41 -0500

    biography and photo uploads, check errors, boolen active site options

--- app/Usercontenttype.php
+++ app/Usercontenttype.php
@@ -5,8 +5,18 @@ use Illuminate\Database\Eloquent\Model;
 class Usercontenttype extends Model {
     protected $guarded = ['id'];
     protected $table ="usercontents";
+    protected $appends = ['activebool'];
+
     public function contenttype(){
         return $this->belongsTo('App\Contenttype');
+    }
+
+    public function getActiveboolAttribute(){
+        $active = $this->getAttribute('active');
+        if($active==1){
+            return true;
+        }
+        return false;
     }
 }
 
